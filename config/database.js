356f3ace82c687b54a6cf905e2ef168ea3a62f6e
Srabo93 <a.s-dev@web.de>
2023-03-11 21:03:32 +0100

    added console logs for node env

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require("sequelize");
-
+require('dotenv').config();
 let sequelize;
 
 if(process.env.NODE_ENV === 'production'){
-   sequelize = new Sequelize(process.env.PSQL_URI)
+   sequelize = new Sequelize(process.env.PSQL_URI,{dialectOptions:{ssl: false}})
+   console.log('production set');
 }else{
+   console.log('developer set');
    sequelize = new Sequelize("E-Shop", "root", "password", {
   host: "db",
   dialect: "postgres",
