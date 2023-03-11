d5b772df43160c461ee0d01c11bff35231950137
Srabo93 <a.s-dev@web.de>
2023-03-11 21:25:31 +0100

    Url was still pointing to localhost

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -57,7 +57,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "http://localhost:8000/create-order",
+      return_url: "https://e-shop-nodejs.onrender.com/create-order",
       receipt_email: emailAddress,
     },
   });
