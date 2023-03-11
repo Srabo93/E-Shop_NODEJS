6b8000c64faf6bd835b5e5190936d4f179d8450b
Srabo93 <a.s-dev@web.de>
2023-03-11 14:28:03 +0100

    reverted string adjustment

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 let sequelize;
 
 if(process.env.NODE_ENV === 'production'){
-   sequelize = new Sequelize(`${process.env.PSQL_URI}`)
+   sequelize = new Sequelize(process.env.PSQL_URI)
 }else{
    sequelize = new Sequelize("E-Shop", "root", "password", {
   host: "db",
