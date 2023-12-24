4dbc766d10dad4bbbf3a3f0509570c082903e765
Srabo93 <a.s-dev@web.de>
2023-12-24 15:36:50 +0100

    added docker

--- config/database.js
+++ config/database.js
@@ -1,17 +1,19 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 let sequelize;
 
-if(process.env.NODE_ENV === 'production'){
-   sequelize = new Sequelize(process.env.PSQL_URI,{dialectOptions:{ssl: false}})
-   console.log('production set');
-}else{
-   console.log('developer set');
-   sequelize = new Sequelize("E-Shop", "root", "password", {
-  host: "db",
-  dialect: "postgres",
-  logging: false,
-  })
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.PSQL_URI, {
+    dialectOptions: { ssl: false },
+  });
+  console.log("production set");
+} else {
+  console.log("developer set");
+  sequelize = new Sequelize("e-shop", "postgres", process.env.DB_PW, {
+    host: "db",
+    dialect: "postgres",
+    logging: false,
+  });
 }
 
 module.exports = sequelize;
